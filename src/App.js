e84744dca95b718df3faa7a167c5af480f6e1d11
hariprasath272 <114482289+hariprasath272@users.noreply.github.com>
2022-09-27 16:23:15 +0530

    todolist project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './Components/style.css';
-import Meal from './Components/Meal';
-function App() {
-  return(
-      <Meal/>
-  )
-  
-}
+import React from "react";
+import { Component } from "react";
+import Todo from "./Todo";
 
-export default App;
+
+
+//   return(
+//     <div>
+//       <h1>Hari uhh</h1>
+//       <h1><Todo/></h1>
+//     </div>
+//   )
+// }
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1><Todo/></h1>
+        {/* <h1>hari</h1> */}
+      </div>
+    )
+  }
+
+}
+export default App
